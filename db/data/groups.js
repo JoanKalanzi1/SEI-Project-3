6f2c6b3f123b83073bb30f4d87731cdbae01063c
joankalanzi <jaon.kalanzi1@gmail.comgit config --global user.name joan>
2021-07-03 16:22:18 +0100

    groups have location

--- db/data/groups.js
+++ db/data/groups.js
@@ -1,7 +1,7 @@
 export default [
   {
     name: "Netball for the youth",
-    image: "img",
+    image: "https://imgur.com/ACV4zY8",
     activity: ["Netball", "Socialising" ],
     location: { name: "Bishopsgate", longitude: -0.07851, latitude: 51.518852 },
     number: "0",
@@ -11,8 +11,8 @@ export default [
     contact: "contact"
   },
   {
-    name: "Cheese loves",
-    image: "img",
+    name: " We love Cheese",
+    image: "https://imgur.com/yZvZdMc",
     activity: ["social", "Eating"],
     location: { name: "	Cock Lane", longitude: 	-0.102727, latitude: 51.516996 },
     number: "0",
@@ -23,7 +23,7 @@ export default [
   },
   {
     name: "Tree hugging meditation",
-    image: "img",
+    image: "https://imgur.com/JdOH5NP",
     activity: ["Nature", "Gardening"],
     location: { name: " Aldersgate Street", longitude: 	-0.097025, latitude: 	51.520737 },
     number: "0",
@@ -34,7 +34,7 @@ export default [
   },
   {
     name: "Walking away",
-    image: "img",
+    image: "https://imgur.com/L5DobTt",
     activity: ["Hiking", "Rambling"],
     location: { name: "Aldgate", longitude: -0.078905, latitude: 	51.514882 },
     number: "0",
@@ -45,7 +45,7 @@ export default [
   },
   {
     name: "Baking warriors",
-    image: "img",
+    image: "https://imgur.com/czDHNq9",
     activity: ["Baking", "Cooking"],
     location: { name: "Chancery Lane ", longitude: -0.112442, latitude: 51.517 },
     number: "0",
@@ -55,8 +55,8 @@ export default [
     contact: "contact"
   },
   {
-    name: "Urban jungle gardening",
-    image: "img",
+    name: "Urban gardening",
+    image: "https://imgur.com/wfJtAZO",
     activity: ["Gardening", "Nature"],
     location: { name: "Ascot Road ", longitude: -0.424507 , latitude: 51.64862},
     number: "0",
@@ -67,7 +67,7 @@ export default [
   },
   {
     name: "The Ultimate Frisbee group",
-    image: "img",
+    image: "https://imgur.com/bIXDZPT",
     activity: ["Ultimate Frisbee"],
     location: { name: "Warbank Crescent", longitude:-0.001272, latitude: 51.338315 },
     number: "0",
@@ -78,7 +78,7 @@ export default [
   },
   {
     name: "Warhammer",
-    image: "img",
+    image: "https://imgur.com/JcsgSBz",
     activity: ["Modeling", "Wargaming", "Painting"],
     location:{ name: "Bothwell Road ", longitude: -0.015246, latitude: 	51.344533 },
     number: "0",
@@ -89,7 +89,7 @@ export default [
   },
   {
     name: "Reading over wine",
-    image: "hey",
+    image: "https://imgur.com/seBAAXV",
     activity: ["Drinks", "Reading"],
     location: { name: " Bedford Gardens", longitude: -0.193568, latitude: 51.506947 },
     number: "0",
@@ -100,7 +100,7 @@ export default [
   },
   {
     name: "Biking",
-    image: "hey",
+    image: "https://imgur.com/WSpz672",
     activity: ["Mountain Biking", "cycling"],
     location: { name: "Lennox Gardens", longitude: -0.164845, latitude: 51.496377 },
     number: "0",
@@ -111,7 +111,7 @@ export default [
   },
   {
     name: "soup kitchen",
-    image: "hey",
+    image: "https://imgur.com/d4BfRhC",
     activity: ["cooking", "volunteering"],
     location: { name: "Annandale Road ", longitude: 0.007598, latitude: 51.486366 },
     number: "0",
@@ -121,19 +121,19 @@ export default [
     contact: "hey"
   },
   {
-    name: "Soup kitchen",
-    image: "hey",
-    activity: "cooking",
+    name: "Video Gaming",
+    image: "https://imgur.com/iM9g9Xs",
+    activity: ["computer Gaming", "Gaming"],
     location: { name: "Bellot Street", longitude: 	0.005707, latitude: 	51.487188 },
     number: "0",
-    about: " we volunteer to various soup centers in london ",
-    time: "09:00",
+    about: " we play games on online together",
+    time: "20:00",
     level: " Beginner",
     contact: "hey"
   },
   {
     name: "Football",
-    image: "hey",
+    image: "https://imgur.com/gdQjwCN",
     activity: ["Football", "Running"],
     location: { name: "Boyfield Street ", longitude: -0.102138, latitude: 51.49928 },
     number: "0",
@@ -144,7 +144,7 @@ export default [
   },
   {
     name: "Singing Angels",
-    image: "hey",
+    image: "https://imgur.com/9yrjrNa",
     activity: ["Music", "Films"],
     location:{ name: "Great Suffolk Street ", longitude: 	-0.098699, latitude: 	51.502983 },
     number: "0",
@@ -155,7 +155,7 @@ export default [
   },
   {
     name: "Dancing for fun",
-    image: "hey",
+    image: "https://imgur.com/HKiHUny",
     activity: ["Dancing", "Music"],
     location: { name: " Mandela Way ", longitude: -0.080152, latitude: 	51.492762 },
     number: "0",
@@ -166,7 +166,7 @@ export default [
   },
   {
     name: "Board games",
-    image: "hey",
+    image: "https://imgur.com/4LXpaEs",
     activity: ["Boarding", "Puzzling"],
     location: { name: "Asolando Drive ", longitude: -0.092679, latitude:	51.490835 },
     number: "0",
@@ -177,7 +177,7 @@ export default [
   },
   {
     name: 'The camera club',
-    image: 'image',
+    image: "https://imgur.com/knOsdhS",
     activity: ["Photography", "Films"], // indoor or outdoor
     location: { name: "West Hampstead Mews ", longitude: 	-0.189936, latitude: 51.547303 },
     number: 'however many you want',
@@ -188,7 +188,7 @@ export default [
   },
   {
     name: "Knitting and movies",
-    image: 'image',
+    image: "https://imgur.com/CdSIaRk",
     activity: ["kitting", "Films"], // indoor or outdoor
     location: { name: "Kingdon Road ", longitude: -0.192392, latitude: 51.549382 },
     number: 'however many you want',
@@ -199,7 +199,7 @@ export default [
   },
   {
     name: "Mindfulness through games",
-    image: 'image',
+    image: "https://imgur.com/2hWiQJD",
     activity: ["puzzling", "Math/ Mind Game", "Board games", "Chess"], // indoor or outdoor
     location: { name: "Great Eastern Road", longitude: 0.003688, latitude: 51.544929 },
     number: 'however many you want',
@@ -210,7 +210,7 @@ export default [
   },
   {
     name: 'We create art',
-    image: 'image',
+    image: "https://imgur.com/ZUYPboU",
     activity: ['Painting'], // indoor or outdoor
     location: { name: "Amhurst Road ", longitude: -0.061674, latitude: 	51.549004 },
     number: 'however many you want',
@@ -221,7 +221,7 @@ export default [
   },
   {
     name: 'Anime Review',
-    image: 'image',
+    image: "https://imgur.com/NwDOzUt",
     activity: ['Reading', 'Films, Music'], // indoor or outdoor
     location: { name: "Benworth Street ", longitude: 	-0.02787, latitude:51.528216 },
     number: '',
@@ -232,7 +232,7 @@ export default [
   },
   {
     name: 'Yoga and wellbeing',
-    image: 'image',
+    image: "https://imgur.com/kHiukfT",
     activity: ['Yoga','Math / Mind Games'], // indoor or outdoor
     location: { name: "Cairo Road ", longitude: 	-0.018014, latitude: 51.585503 },
     number: '',
@@ -243,7 +243,7 @@ export default [
   },
   {
     name: 'The sailing club',
-    image: 'image',
+    image: "https://imgur.com/rZMkYI1",
     activity: ['Swimming', 'Sailing'], // indoor or outdoor
     location: { name: "Harrow Lane ", longitude: 	-0.0131, latitude:	51.508507 },
     number: '',

84fa7179894f18698004568218316622f93d9f8b
joankalanzi <jaon.kalanzi1@gmail.comgit config --global user.name joan>
2021-07-08 20:44:49 +0100

    centred logo

--- client/src/components/Home/NavBar.js
+++ client/src/components/Home/NavBar.js
@@ -1,6 +1,6 @@
 // import Logo from '../styles/Logo.png'
 import Navbar from 'react-bootstrap/Navbar'
-import { Nav, Modal, Image } from 'react-bootstrap'
+import { Nav, Modal, Image, Container, Col } from 'react-bootstrap'
 import { LinkContainer } from 'react-router-bootstrap'
 import React, { useState } from 'react'
 
@@ -30,32 +30,41 @@ const NavBar = () => {
   // { window.location.pathname === '/Login' ? 'Login Now' : 'Register Now' }
   return (
     <>
+
       <Navbar className="navbar-colour" Navbar bg="" variant="dark" >
-        <div className="logo-nav-items">
-          <div className="logo">
-            <Navbar.Brand href="#home"><Image src="https://imgur.com/0C9foJS.jpeg" /></Navbar.Brand>
-          </div>
-          <div className="navbar-items">
-            <Nav className="mr-auto">
-              <LinkContainer to='/'>
-                <Nav.Link href="#home">Home</Nav.Link>
-              </LinkContainer>
-              <LinkContainer to='/activities'>
-                <Nav.Link href="#activities">Activities</Nav.Link>
-              </LinkContainer>
-              <LinkContainer to='/groups'>
-                <Nav.Link href="#groups">Groups</Nav.Link>
-              </LinkContainer>
-              <LinkContainer to='/Login'>
-                <Nav.Link href="#login" onClick={handleShow}>Login</Nav.Link>
-              </LinkContainer>
-              <LinkContainer to='#' variant="primary" onClick={handleShow}>
-                <Nav.Link href="#register">Register</Nav.Link>
-              </LinkContainer>
-            </Nav>
-          </div>
-        </div>
+        < Container >
+
+          <Col></Col>
+          <Col>
+            <div className="logo-nav-items">
+              <div className="logo">
+                <Navbar.Brand href="#home"><Image src="https://imgur.com/0C9foJS.jpeg" /></Navbar.Brand>
+              </div>
+              <div className="navbar-items">
+                <Nav className="mr-auto">
+                  <LinkContainer to='/'>
+                    <Nav.Link href="#home">Home</Nav.Link>
+                  </LinkContainer>
+                  <LinkContainer to='/activities'>
+                    <Nav.Link href="#activities">Activities</Nav.Link>
+                  </LinkContainer>
+                  <LinkContainer to='/groups'>
+                    <Nav.Link href="#groups">Groups</Nav.Link>
+                  </LinkContainer>
+                  <LinkContainer to='/Login'>
+                    <Nav.Link href="#login" onClick={handleShow}>Login</Nav.Link>
+                  </LinkContainer>
+                  <LinkContainer to='#' variant="primary" onClick={handleShow}>
+                    <Nav.Link href="#register">Register</Nav.Link>
+                  </LinkContainer>
+                </Nav>
+              </div>
+            </div>
+          </Col>
+          <Col></Col>
+        </Container>
       </Navbar>
+
       <section>
         <Modal
           show={show}
@@ -68,8 +77,8 @@ const NavBar = () => {
           </Modal.Header>
           <Modal.Body>Select from the options below</Modal.Body>
           <Modal.Footer>
-            <Link onClick={() => handleClose()} to= {location.pathname === '/Login' ? '/Login' : '/Register'}>User</Link>
-            <Link onClick={() => handleClose()} to= {location.pathname === '/Login' ?  '/grouplogin' : '/groupregister'}>Group</Link>
+            <Link onClick={() => handleClose()} to={location.pathname === '/Login' ? '/Login' : '/Register'}>User</Link>
+            <Link onClick={() => handleClose()} to={location.pathname === '/Login' ? '/grouplogin' : '/groupregister'}>Group</Link>
           </Modal.Footer>
         </Modal>
       </section>

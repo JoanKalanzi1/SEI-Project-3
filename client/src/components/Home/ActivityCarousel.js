bf15a734ce9ac83c2a761eb74a574310fc07d194
Mike Soltysiak <mtsolt@outlook.com>
2021-07-08 11:00:52 +0100

    group single updated

--- client/src/components/Home/ActivityCarousel.js
+++ client/src/components/Home/ActivityCarousel.js
@@ -35,6 +35,7 @@ const ActivityCarousel = () => {
     getActivityData()
   }, [])
   console.log('ACTIVITIES', activities)
+  
   useEffect(() => {
     const getCarouselData = () => {
       for (let i = 0; i < 8; i++) {
--- client/src/components/groups/SingleGroup.js
+++ client/src/components/groups/SingleGroup.js
@@ -3,10 +3,15 @@ import axios from 'axios'
 import { useParams } from 'react-router-dom'
 
 const SingleGroup = () => {
-  const [group, setGroup] = useState([])
-  const [location, setLocation] = useState([])
+  const [group, setGroup] = useState(null) // linked to first getData
+  const [location, setLocation] = useState([]) // also linked to first getData
+  const [groupActivityData, setGroupActivityData] = useState([]) // also linked to first getData
+  const [num, setNum] = useState([null]) // also linked to first getData
+  const [activities, setActivities] = useState(null) //
+  const [groupActivities, setGroupActivities] = useState(null)
   const [hasError, setHasError] = useState(false)
   const { id } = useParams()
+  const activitiesArray = []
 
   useEffect(() => {
     const getData = async () => {
@@ -14,25 +19,75 @@ const SingleGroup = () => {
         const { data } = await axios.get(`/api/groups/${id}`)
         setGroup(data)
         setLocation(data.location)
-        console.log('GROUP', group)
-        console.log('DATA', data)
-
+        setGroupActivityData(data.activity)
+        
       } catch (err) {
         setHasError(true)
         console.log('ERROR WHILE GETTING GROUP DATA', err)
       }
     }
     getData()
+    console.log('getDATA RUNNING')
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [id])
 
-  console.log('DATA', group)
+  useEffect(() => {
+    const setInfo = () => {
+      setNum(groupActivityData.length)
+    }
+    setInfo()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [group])
+
+  useEffect(() => {
+    const getActivityData = async () => {
+      try {
+        const { data } = await axios.get('/api/activities')
+        setActivities(data)
+      } catch (err) {
+        setHasError(true)
+        console.log('ERROR WHILE GETTING ACTIVITY DATA', err)
+      }
+    }
+    if (num) getActivityData()
+    console.log('getACTIVITYDATA RUNNING')
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [num])
+
+
+  useEffect(() => {
+    const getGroupActivities = () => {
+      console.log('getGroupActivities RUNNING')
+      for (let i = 0; i < num; i++) {
+        const activityFound = activities.nameOfActivity.filter(groupActivityData[i])
+        console.log('ACTIVITYFOUND', activityFound)
+        activitiesArray.push(activityFound)
+      }
+      setGroupActivities(activitiesArray)
+    }
+    if (activities) getGroupActivities()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [activities])
+  if (!groupActivities) return null
+
+  // var bedrooms = myArray.filter(name => name.includes('bedroom'))
+
+  console.log('GROUP', group)
+  console.log('GroupActivityData', groupActivityData)
+  console.log('activities set!', activities)
+  console.log('groupActivities', groupActivities)
+  console.log('number set', num)
+
+
   return (
     <section>
       {group ?
         <div>
           <h2>{group.name}</h2>
           <h4>{location.name}</h4>
+          <h3>{group.about}</h3>
+
+
         </div>
         :
         <h2>
